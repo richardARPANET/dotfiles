8e15eb54dfa37d85c811bad8e5bba24f6a59aa49
Richard O'Dwyer <richard@richard.do>
2015-05-11 20:07:24 +0100

    Use araxis for git comparisons. Adds copy to home bash script for easy updating

--- .bash_aliases
+++ .bash_aliases
@@ -1,5 +1,4 @@
 alias v="vagrant"
 alias g="git"
 alias ll="ls -lG"
-alias gcur="git br | grep '*'"
 alias gchp='_(){ git checkout -b refs/pull/origin/$1; }; _'
---
+++ copy_to_home.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cp .bash_aliases ~/.bash_aliases
+cp .bashrc ~/.bashrc
+cp .git-completion.bash ~/.git-completion.bash
+cp .gitconfig ~/.gitconfig
